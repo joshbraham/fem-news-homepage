bf1a999d48fe92ce7fc693a4e137055a124a434c
areallybadcoder <josh27ofto@gmail.com>
2023-03-29 04:23:04 +0100

    Smoothen overlay transition

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const openMenuBtn = document.querySelector(".btn-open-menu");
 const closeMenuBtn = document.querySelector(".btn-close-menu");
 
 openMenuBtn.addEventListener("click", () => {
-  navOverlay.style.opacity = 1;
   navOverlay.style.zIndex = 1;
   navMenu.style.translate = "0";
+  navOverlay.style.opacity = 1;
 });
 
 closeMenuBtn.addEventListener("click", () => {
   navOverlay.style.opacity = 0;
-  navOverlay.style.zIndex = -1;
   navMenu.style.translate = "100%";
+  navOverlay.style.zIndex = -1;
 });
